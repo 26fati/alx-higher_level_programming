c543dadb00760bd397ee41034489e4e4d6b016e3
26fati <fatibeautiful1@gmail.com>
2023-08-08 10:18:25 +0100

    task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
     if i % 2 != 0:
-        print(chr(i).upper(), end='')
+        print("{}".format(chr(i).upper()), end='')
     else:
-        print(chr(i), end='')
+        print("{}".format(chr(i)), end='')
