16363ec2534d99f3bf23077c74f43c7a9e036d1e
26fati <fatibeautiful1@gmail.com>
2023-08-08 10:27:12 +0100

    task 14

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -18,7 +18,7 @@ listint_t *insert_node(listint_t **head, int number)
 	new->n = number;
 	new->next = NULL;
 	if (*head == NULL)
-		*head = new
+		*head = new;
 	else
 	{
 	current = (*head)->next;
