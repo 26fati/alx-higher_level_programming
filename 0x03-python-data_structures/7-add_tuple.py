2d2cbc1f59de04f926a9f38d5b510a694f69db32
26fati <fatibeautiful1@gmail.com>
2023-08-12 20:36:09 +0100

    task 13

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,6 +7,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len2 < 2:
         tuple_b = tuple_b + (0, 0, )
     element1 = tuple_a[0] + tuple_b[0]
-    element2= tuple_a[1] + tuple_b[1]
+    element2 = tuple_a[1] + tuple_b[1]
     new_tuple = (element1, element2)
     return new_tuple
