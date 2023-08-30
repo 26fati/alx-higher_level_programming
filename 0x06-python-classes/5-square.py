8b52d855c91d0def0def1f19200119750f7e4fd2
26fati <fatibeautiful1@gmail.com>
2023-08-30 20:24:31 +0100

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -4,21 +4,26 @@
 
 class Square:
     """square class with it's size and proper validation"""
+
+
     def __init__(self, size=0):
         self.__size = size
 
     """method to calculate the area of a square"""
+
 
     def area(self):
         return self.__size ** 2
 
     """get the private property"""
 
+
     @property
     def size(self):
         return self.__size
     
     """setter for size"""
+
 
     @size.setter
     def size(self, value):
@@ -28,7 +33,8 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-     """print the square"""
+    """print the square"""
+
 
     def my_print(self):
         if self.__size = 0:
