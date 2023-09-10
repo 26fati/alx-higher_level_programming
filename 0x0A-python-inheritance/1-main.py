809d78cc4b206906375d119e21827f2f180870d5
26fati <fatibeautiful1@gmail.com>
2023-09-10 11:55:07 +0100

    tasks

---
+++ 0x0A-python-inheritance/1-main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+MyList = __import__('1-my_list').MyList
+
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
+print(type(my_list))
+lst = [3, 4, 5]
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
+print(inherits_from(lst, list))
