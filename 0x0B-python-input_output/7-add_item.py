1292b206b06978a6217763e7188753ea60860bf4
26fati <fatibeautiful1@gmail.com>
2023-09-12 11:06:46 +0100

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Add all arguments to a Python list and save them to a file."""
 import sys
+
+
 if __name__ == "__main__":
     load_from_json_file = \
             __import__('6-load_from_json_file').load_from_json_file
