648400413fe9e62b3e4579b72aac96e97be1b257
26fati <fatibeautiful1@gmail.com>
2023-11-13 22:52:06 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@
 
 const argv = process.argv;
 if (!argv[2] || argv.length === 3) { console.log('0'); } else {
-  const arr = [];
+  let arr = [];
   for (let i = 2; i < argv.length; i++) { arr.push(Number(argv[i])); }
   arr.sort();
   console.log(arr[arr.length - 2]);
