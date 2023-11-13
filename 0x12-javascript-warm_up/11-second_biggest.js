0a8548a2564d89de6f3b413ad972eb2cc5cf7a31
26fati <fatibeautiful1@gmail.com>
2023-11-13 22:55:19 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,8 +3,8 @@
 // a script that searches the second biggest integer in the list of arguments.
 
 const argv = process.argv;
+const arr = [];
 if (!argv[2] || argv.length === 3) { console.log('0'); } else {
-  let arr = [];
   for (let i = 2; i < argv.length; i++) { arr.push(Number(argv[i])); }
   arr.sort();
   console.log(arr[arr.length - 2]);
