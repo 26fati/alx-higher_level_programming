53022987fe180ef12b15a0fa894383363dbeef31
26fati <fatibeautiful1@gmail.com>
2023-11-14 13:43:25 +0100

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+// const Rectangle = require('./4-rectangle');
 
-class Square extends Rectangle {
+class Square extends require('./5-square.js') {
   constructor (size) {
     super(size, size);
   }
 
   charPrint (c) {
-    if (c === undefined) { this.print(); } else {
+    if (c === undefined) { super.print(); } else {
       for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); }
     }
   }
