5b07f89492c6a14f1293b8230725b0509bbf47bb
26fati <fatibeautiful1@gmail.com>
2023-11-30 23:01:04 +0100

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers
+"""
+
+
 def find_peak(list_of_integers):
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
+    """
     n = len(list_of_integers)
     if (list_of_integers == None or n == 0):
          return None
