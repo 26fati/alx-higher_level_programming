5459dac3344c5c16e0e9d192edb1a87b855e5dad
26fati <fatibeautiful1@gmail.com>
2023-12-01 20:09:58 +0100

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    try:
+        value = {'q': sys.argv[1]}
+    except IndexError:
+        value = {'q': ''}
+    r = requests.post('http://52.204.190.21:5000/search_user', data=value)
+    try:
+        response = r.json()
+        if response == {}:
+            print('No result')
+        else:
+            print(f'[{response["id"]}] {response["name"]}')
+    except ValueError:
+        print("Not a valid JSON")
