c00e3980b652c32273ef26d45fa7ef1d3b2a98c6
26fati <fatibeautiful1@gmail.com>
2024-01-03 21:08:54 +0100

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const path = process.argv[3];
+request(url, (err, req, body) => {
+  if (err) {
+    console.log(err);
+  }
+  fs.writeFile(path, body, 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
