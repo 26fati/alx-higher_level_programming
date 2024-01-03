ab269d3e02de2aa3ab67a410a358d3a221ba992e
26fati <fatibeautiful1@gmail.com>
2024-01-03 18:54:36 +0100

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request(url, (error, response, body) => {
-  console.log(error || `Code: ${response.statusCode}`);
+request(url, (error, response) => {
+  console.log(error || `code: ${response.statusCode}`);
 });
